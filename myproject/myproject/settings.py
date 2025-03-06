5c490f7b937a3dc3f4c1ec9c6aab421876cc8e8b
Musefiu Ojo Amos <amosolayemi18@gmail.com>
2025-03-06 18:05:34 +0000

    another changes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6%o@kt9t+2i)vjp(a3d1$1thx(n_d=34+m*1$5n0(5ht-ogf2x'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-6%o@kt9t+2i)vjp(a3d1$1thx(n_d=34+m*1$5n0(5ht-ogf2x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split("")
 
 
 # Application definition
@@ -94,6 +96,10 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL")
+
+DATABASES["default"] = dj_database_url.parse(database_url)
+#"postgresql://amos_portfolio_user:UdABBftGnCIOeAjljltdIW062lKGSkK2@dpg-cv4q5ol2ng1s73bqjl90-a.oregon-postgres.render.com/amos_portfolio"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

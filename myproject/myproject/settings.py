2594171e90476f27e5188ec3d2fc99bd900a7147
Musefiu Ojo Amos <amosolayemi18@gmail.com>
2025-03-06 21:53:06 +0000

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import dj_database_url
 
 from pathlib import Path
 
@@ -22,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
-# 'django-insecure-6%o@kt9t+2i)vjp(a3d1$1thx(n_d=34+m*1$5n0(5ht-ogf2x'
+SECRET_KEY = 'django-insecure-6%o@kt9t+2i)vjp(a3d1$1thx(n_d=34+m*1$5n0(5ht-ogf2x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -96,15 +94,6 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASE_URL")
-
-if database_url:
-    DATABASES["default"] = dj_database_url.parse(database_url)
-else:
-    raise ValueError("DATABASE_URL is not set properly")
-
-DATABASES["default"] = dj_database_url.parse(database_url)
-#"postgresql://amos_portfolio_user:UdABBftGnCIOeAjljltdIW062lKGSkK2@dpg-cv4q5ol2ng1s73bqjl90-a.oregon-postgres.render.com/amos_portfolio"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
